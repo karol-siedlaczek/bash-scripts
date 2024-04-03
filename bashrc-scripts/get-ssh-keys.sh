6ff2be0249397e51bc4793b541f46d09dff1a5a0
Karol Siedlaczek <31355807+karol-siedlaczek@users.noreply.github.com>
2024-04-03 17:58:59 +0200

    Update get-ssh-keys.sh

--- bashrc-scripts/get-ssh-keys.sh
+++ bashrc-scripts/get-ssh-keys.sh
@@ -6,7 +6,7 @@ WEASEL_PATH="windows/path/to/weasel-pageant-1.4"
 echo -n "Loading SSH keys from pageant... "
 "$WEASEL_PATH/weasel-pageant" -k> /dev/null 2> /dev/null
 eval $("$WEASEL_PATH/weasel-pageant" -r -a "/tmp/.weasel-pageant-$USER-$RANDOM")> /dev/null 2> /dev/null
-sleep 1
+
 ssh_keys_loaded=$(ssh-add -l | grep -c SHA)
 
 if [[ $ssh_keys_loaded -gt 0 ]];  then
