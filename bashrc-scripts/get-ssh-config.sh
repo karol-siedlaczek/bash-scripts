d4516a61336125bb5d3951e34bc394c157bef0b4
Karol Siedlaczek <31355807+karol-siedlaczek@users.noreply.github.com>
2024-08-13 14:05:02 +0200

    Update get-ssh-config.sh

--- bashrc-scripts/get-ssh-config.sh
+++ bashrc-scripts/get-ssh-config.sh
@@ -2,21 +2,23 @@
 
 REPO_PATH="<REPO_URL>"
 REPO_TOKEN="<OAUTH2_TOKEN>"
-TMP_DIR="/tmp/.ssh-config-${RANDOM}-${USER}.repo"
 
-[ -d $TMP_DIR ] && rm -rf $TMP_DIR
+tmp_dir="/tmp/.ssh-config-${RANDOM}-${USER}.repo"
+
+[ -d $tmp_dir ] && rm -rf $tmp_dir
 
 echo -n "Cloning ssh config files... "
 REPO="https://oauth2:${REPO_TOKEN}@${REPO_PATH}"
 
-if ! git clone --quiet $REPO $TMP_DIR > /dev/null
+if ! git clone --quiet $REPO $tmp_dir > /dev/null
 then
         echo "Failed to load ssh config from '${REPO_PATH}' repo"
 else
         mkdir -p ~/.ssh/config.d
-        mv $TMP_DIR/config.d/* ~/.ssh/config.d/
-        cat $TMP_DIR/config > ~/.ssh/config
+        mv $tmp_dir/config.d/* ~/.ssh/config.d/
+        cat $tmp_dir/config > ~/.ssh/config
         chmod 700 ~/.ssh/config.d && chmod 600 ~/.ssh/config ~/.ssh/config.d/*
-        rm -rf $TMP_DIR
+        dos2unix --quiet ~/.ssh/config
+        rm -rf $tmp_dir
         echo "success"
 fi
